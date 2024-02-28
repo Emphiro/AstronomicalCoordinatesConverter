dffb3c2d542288fdc63863106f6e82e6ed6ec1ce
Emphiro <124156993+Emphiro@users.noreply.github.com>
2024-02-28 15:32:08 +0100

    Minimum viable Product
    A simple Version of the converter with only the necessary functions

--- converter.py
+++ converter.py
@@ -112,76 +112,18 @@ def get_time_jd() -> float:
     return current_config.time.jd
 
 
-def set_time(*new_time: [int, int, int, int, int, float], single_value=False) -> Time:
-    """new_time as *[year, month, day, hour, minute, second]"""
-    if single_value:
-        set_time_utc(new_time[0])
-    global use_current_time
-    use_current_time = False
-    global current_config
-    current_config.time = normal_time_to_utc(
-        new_time[0], new_time[1], new_time[2], new_time[3], new_time[4], new_time[5])
-    return current_config.time
-
-
-def set_time_utc(new_time: Time):
-    """new_time as Time object"""
-    global use_current_time
-    use_current_time = False
-    global current_config
-    current_config.time = new_time
-
-
-def set_ra(*new_ra: [float, float, float], single_value=False) -> str:
-    """new_ra as *[hours, minutes, seconds]"""
-    global current_config
-    if single_value:
-        current_config.ra = new_ra[0]
-        return deg_to_hms(current_config.ra)
-    current_config.ra = hms_to_deg(new_ra[0], new_ra[1], new_ra[2])
-    return deg_to_hms(current_config.ra)
 
 
 def get_ra() -> float:
     return current_config.ra
 
 
-def set_dec(*new_dec: [float, float, float], single_value=False) -> str:
-    """new_dec as *[degrees, arcminutes, arcseconds]"""
-    global current_config
-    if single_value:
-        current_config.dec = new_dec[0]
-        return deg_to_dms(current_config.dec)
-    current_config.dec = dms_to_deg(new_dec[0], new_dec[1], new_dec[2])
-    return deg_to_dms(current_config.dec)
-
-
 def get_dec() -> float:
     return current_config.dec
 
 
-def set_lon(*new_lon: [float, float, float], single_value=False) -> str:
-    """new_lon as *[degrees, arcminutes, arcseconds]"""
-    global current_config
-    if single_value:
-        current_config.lon = new_lon[0]
-        return deg_to_dms(current_config.lon)
-    current_config.lon = dms_to_deg(new_lon[0], new_lon[1], new_lon[2])
-    return deg_to_dms(current_config.lon)
-
-
 def get_lon():
     return current_config.lon
-
-
-def set_lat(*new_lat: [float, float, float], single_value=False) -> str:
-    """new_lat as *[degrees, arcminutes, arcseconds]"""
-    global current_config
-    if single_value:
-        current_config.lat = new_lat[0]
-        return deg_to_dms(current_config.lat)
-    current_config.lat = dms_to_deg(new_lat[0], new_lat[1], new_lat[2])
-    return deg_to_dms(current_config.lat)
 
 
 def get_lat() -> float:
@@ -261,330 +203,57 @@ class Configuration:
                 f"\nLatitude: {deg_to_dms(self.lat)}")
 
 
-# The Example testcases
-
-examples_old = [
-    Configuration(
-        time=normal_time_to_utc(2021, 2, 7, 22, 23, 24),
-        ra=hms_to_deg(18, 18, 48),
-        dec=dms_to_deg(-13, 48, 24),
-        lon=dms_to_deg(10, 53, 22),
-        lat=dms_to_deg(49, 53, 6)),
-
-    Configuration(
-        time=normal_time_to_utc(2021, 1, 7, 0, 0, 0),
-        ra=hms_to_deg(2, 31, 49.09),
-        dec=dms_to_deg(89, 15, 50.8),
-        lon=dms_to_deg(10, 53, 22),
-        lat=dms_to_deg(49, 53, 6)),
-
-    Configuration(
-        time=normal_time_to_utc(2020, 12, 23, 7, 34, 34.5),
-        ra=hms_to_deg(13, 37, 0.919),
-        dec=dms_to_deg(-29, 51, 56.74),
-        lon=dms_to_deg(10, 53, 22),
-        lat=dms_to_deg(49, 53, 6)),
-
-]
-
-configurations = [
-    Configuration(
-        time=normal_time_to_utc(2020, 12, 23, 7, 34, 34.5),
-        ra=hms_to_deg(13, 37, 0.919),
-        dec=dms_to_deg(-29, 51, 56.74),
-        lon=dms_to_deg(10, 53, 22),
-        lat=dms_to_deg(49, 53, 6)),
-
-    Configuration(
-        time=normal_time_to_utc(2021, 2, 7, 22, 23, 24),
-        ra=hms_to_deg(18, 18, 48),
-        dec=dms_to_deg(-13, 48, 24),
-        lon=dms_to_deg(10, 53, 22),
-        lat=dms_to_deg(49, 53, 6)),
-
-    Configuration(
-        time=normal_time_to_utc(2021, 1, 7, 0, 0, 0),
-        ra=hms_to_deg(2, 31, 49.09),
-        dec=dms_to_deg(89, 15, 50.8),
-        lon=dms_to_deg(10, 53, 22),
-        lat=dms_to_deg(49, 53, 6)),
-
-]
-
-
 # I/O functions
-
-def save_to_file():
-    with open('configurations.pkl', 'wb') as outp:
-        pickle.dump(configurations, outp, 0)
-
-
-def load_from_file():
-    with open('configurations.pkl', 'rb') as inp:
-        global configurations
-        configurations = pickle.load(inp)
-
-
-def add_config(input_string):
-    ex = Configuration(
-        time=get_time(),
-        ra=get_ra(),
-        dec=get_dec(),
-        lon=get_lon(),
-        lat=get_lat())
-    configurations.append(ex)
-    print("Saved as Configuration {}".format(len(configurations)))
-
-
-def remove_config(input_string):
-    """removes a configuration from the array"""
-    global configurations
-    num = input_string.split()[1] if len(input_string.split()) > 1 else None
-    if num is None:
-        num = input("Which configuration? ")
-    try:
-        num = int(num) - 1
-    except ValueError:
-        print("Please specify a configuration between 1 and {}".format(len(configurations)))
-        return
-    if len(configurations) > num >= 0:
-        confirmation = input("Are you sure you want to delete configuration {}? (y/n)".format(num+1))
-        if confirmation == "Y" or confirmation == "y":
-            configurations.pop(num)
-        else:
-            return
-    else:
-        print("Please specify a configuration between 1 and {}".format(len(configurations)))
-        return
-    print("Successfully removed the specified configuration")
-
-
-def load(input_string):
-    """loads a configuration to be the currently used value"""
-    num = input_string.split()[1] if len(input_string.split()) > 1 else None
-    if num is None:
-        num = input("Which configuration? ")
-    try:
-        num = int(num) - 1
-    except ValueError:
-        print("Please specify a configuration between 1 and {}".format(len(configurations)))
-        return
-    if len(configurations) > num >= 0:
-        ex = configurations[num]
-        set_time_utc(new_time=ex.time)
-        set_ra(ex.ra, single_value=True)
-        set_dec(ex.dec, single_value=True)
-        set_lon(ex.lon, single_value=True)
-        set_lat(ex.lat, single_value=True)
-    else:
-        print("Please specify a configuration between 1 and {}".format(len(configurations)))
-        return
-    list_values("")
 
 
 def print_solution(era, az, el):
     print("Era: {:.4f}\nAzimuth: {}\nElevation: {}".format(era, az, el))
 
 
-def change_value(input_string):
-    types = {
-        "clat": [set_lat, "dms", "Latitude"],
-        "clon": [set_lon, "dms", "Longitude"],
-        "cdec": [set_dec, "dms", "Declination"],
-        "cra": [set_ra, "hms", "Right Ascension"],
-        "ctime": [set_time, "time", "Time"]
-    }
-    questions_list = {
-        "hms": ["hours? ", "minutes? ", "seconds? "],
-        "dms": ["degrees? ", "arcminutes? ", "arcseconds? "],
-        "time": ["year? ", "month? ", "day? ", "hour? ", "minute? ", "second? "]
-    }
-
-    input_arr = input_string.split()
-    set_func = types[input_arr[0]][0]
-    request_type = types[input_arr[0]][1]
-    name = types[input_arr[0]][2]
-
-    questions = questions_list[request_type]
-    values = [0, 0, 0, 0, 0, 0]
-    in_degrees = (len(input_arr) == 2) and request_type != "time"
-    try:
-        if in_degrees:
-            values[0] = float(input_arr[1])
-        elif 1 < len(input_arr) <= len(questions) + 1:
-            for [i, value] in enumerate(input_arr[1:]):
-                values[i] = int(value)
-        else:
-            for [i, question] in enumerate(questions[:-1]):
-                answer = input(question)
-                if answer == '':
-                    values[i] = 0
-                else:
-                    values[i] = int(answer)
-            answer = input(questions[-1])
-            if answer == '':
-                values[len(questions) - 1] = 0
-            else:
-                values[len(questions) - 1] = float(answer)
-
-    except ValueError as er:
-        print(er)
-        print("Invalid Value. Please try again")
-        change_value(input_string.split()[0])
-        return
-    except EOFError:
-        return
-    try:
-        value = set_func(*values, single_value=in_degrees)
-    except ValueError as er:
-        print(er)
-        print("Invalid Value. Please try again")
-        change_value(input_string.split()[0])
-        return
-    print("Set {} to {}".format(name, value))
-
-
-def reset_location(input_string):
-    set_lon(10, 53, 22)
-    set_lat(49, 53, 6)
-    print("set Location to remeis observatory")
-
-
-def reset_time(input_string):
-    global use_current_time
-    use_current_time = True
-    print("set Time to current time")
-
-
-def list_values(input_string):
-    print("Time set to {}".format(get_time()))
-    print("Right Ascension set to {}".format(deg_to_hms(get_ra())))
-    print("Declination set to {}".format(deg_to_dms(get_dec())))
-    print("Longitude set to {}".format(deg_to_dms(get_lon())))
-    print("Latitude set to {}".format(deg_to_dms(get_lat())))
-
-
-def list_configurations(input_string):
-    for (i, ex) in enumerate(configurations):
-        print("Configuration {}:".format(i+1))
-        print(textwrap.indent(str(ex), "    "))
-
-
-def execute(input_string):
-    if len(input_string.split()) > 1:
-        exec_configuration(input_string.split()[1])
-        return
-    time_jd = get_time_jd()
-    ra_deg = get_ra()
-    dec_deg = get_dec()
-    lon_deg = get_lon()
-    lat_deg = get_lat()
+def execute():
+    """prints the era azimuth and elevation for the current configuration"""
+    ex = current_config
+    time_jd = ex.time.jd
+    ra_deg = ex.ra
+    dec_deg = ex.dec
+    lon_deg = ex.lon
+    lat_deg = ex.lat
     [era, az, el] = compute_azimuth_elevation(time_jd, ra_deg, dec_deg, lon_deg, lat_deg)
     print_solution(era, az, el)
 
 
-def change_output_mode(input_string):
-    global output_degrees
-    output_degrees = not output_degrees
-    output = "Output will be given in degrees" if output_degrees else \
-        "Output will be given in degrees arc-minutes and arc-seconds"
-    print(output)
-
-
-def exec_configuration(num):
-    """prints the configuration specified by the integer num"""
-    if num is None:
-        num = input("Which configuration? ")
-    try:
-        num = int(num) - 1
-    except ValueError:
-        print("Please specify an configuration between 1 and {}".format(len(configurations)))
-        return
-    if len(configurations) > num >= 0:
-        ex = configurations[num]
-        time_jd = ex.time.jd
-        ra_deg = ex.ra
-        dec_deg = ex.dec
-        lon_deg = ex.lon
-        lat_deg = ex.lat
-        [era, az, el] = compute_azimuth_elevation(time_jd, ra_deg, dec_deg, lon_deg, lat_deg)
-        print_solution(era, az, el)
-    else:
-        print("Please specify an configuration between 1 and {}".format(len(configurations)))
-
-
-def print_help(input_string):
-    print("Available commands:")
-    for [command, description] in available_commands.items():
-        print(command + ":")
-        print(textwrap.indent(description, "    "))
-
-
-def init_values():
-    global use_current_time
-    use_current_time = True
-    set_time(2020, 12, 23, 7, 34, 34.5)
-    set_ra(13, 37, 0.919)
-    set_dec(-29, 51, 56.74)
-    set_lon(10, 53, 22)
-    set_lat(49, 53, 6)
-
-
-def not_available(input_string):
-    print("Someone has not yet implemented this function :(")
-
-
-available_commands = {
-    "ctime": "Change the time",
-    "cra": "Change the right ascension",
-    "cdec": "Change the declination",
-    "clon": "Change the longitude",
-    "clat": "Change the latitude",
-    "rloc": "Change the location to remeis observatory",
-    "rtime": "Change the time to the current time",
-    "ex": "Calculate the Era, Azimuth and Elevation for the currently set values",
-    "ls": "Show the currently set values",
-    "save": "Save the current configurations",
-    "load": "Load one of the saved configurations",
-    "lse": "List all the saved configurations",
-    "rm": "Remove the selected configuration",
-    "co": "Toggles output mode between degrees and degree:arc-minute:arc-second",
-}
-
-executable_commands = {
-    "help": print_help,
-    "ctime": change_value,
-    "cra": change_value,
-    "cdec": change_value,
-    "clon": change_value,
-    "clat": change_value,
-    "rloc": reset_location,
-    "rtime": reset_time,
-    "ex": execute,
-    "ls": list_values,
-    "save": add_config,
-    "load": load,
-    "lse": list_configurations,
-    "rm": remove_config,
-    "co": change_output_mode,
-}
-
-
-def input_loop():
-    input_string = input("> ")
-    if len(input_string) == 0:
-        return
-    try:
-        command = executable_commands[input_string.split()[0]]
-        command(input_string)
-    except KeyError:
-        print("No matching command found. Type help for a list of commands")
-
-
 # Global Variables (set in init_values)
 
+
 output_degrees = False
-use_current_time = True
+use_current_time = False
+# The Example testcases
+
+
+configurations = [
+    Configuration(
+        time=normal_time_to_utc(2021, 2, 7, 22, 23, 24),
+        ra=hms_to_deg(18, 18, 48),
+        dec=dms_to_deg(-13, 48, 24),
+        lon=dms_to_deg(10, 53, 22),
+        lat=dms_to_deg(49, 53, 6)),
+
+    Configuration(
+        time=normal_time_to_utc(2021, 1, 7, 0, 0, 0),
+        ra=hms_to_deg(2, 31, 49.09),
+        dec=dms_to_deg(89, 15, 50.8),
+        lon=dms_to_deg(10, 53, 22),
+        lat=dms_to_deg(49, 53, 6)),
+
+    Configuration(
+        time=normal_time_to_utc(2020, 12, 23, 7, 34, 34.5),
+        ra=hms_to_deg(13, 37, 0.919),
+        dec=dms_to_deg(-29, 51, 56.74),
+        lon=dms_to_deg(10, 53, 22),
+        lat=dms_to_deg(49, 53, 6)),
+
+]
+
 current_config = Configuration(
         time=normal_time_to_utc(2021, 1, 7, 0, 0, 0),
         ra=hms_to_deg(2, 31, 49.09),
@@ -594,40 +263,7 @@ current_config = Configuration(
 
 
 def main():
-    global DEBUG_MODE
-    if len(sys.argv) > 1:
-        arg = sys.argv[1]
-        if arg == "-e":
-            exec_configuration((sys.argv[2] if len(sys.argv) > 2 else None))
-            return
-        if arg == "-h":
-            print_help("")
-            return
-        if arg == "-d":
-            DEBUG_MODE = True
-    init_values()
-
-    if not DEBUG_MODE:
-        try:
-            load_from_file()
-        except FileNotFoundError:
-            global configurations
-            configurations = examples_old
-            print("No file was found. Loading basic configurations", file=sys.stderr)
-    else:
-        configurations = examples_old
-    print("Welcome to this coordinates converter program!")
-    print()
-    print("Type help for a list of commands")
-    while 1:
-        try:
-            input_loop()
-        except EOFError:
-            break
-        except KeyboardInterrupt:
-            break
-    if not DEBUG_MODE:
-        save_to_file()
+    execute()
 
 
 if __name__ == "__main__":
--- test_converter.py
+++ test_converter.py
@@ -42,43 +42,6 @@ def test_conversions():
     assert str(conv.normal_time_to_utc(2023, 1, 23, 12, 3, 1)) == '2023-01-23T12:03:01.000'
 
 
-def test_getter_setter():
-    num_tests = 500
-    ra = conv.get_ra()
-    dec = conv.get_dec()
-    lat = conv.get_lat()
-    lon = conv.get_lon()
-
-    for i in range(num_tests):
-        deg = floor(uniform(0, 359))
-        hour = floor(uniform(0, 23))
-        min = floor(uniform(0, 59))
-        sec = floor(uniform(0, 59))
-        conv.set_dec(deg, min, sec)
-        assert equals(conv.get_dec(), conv.dms_to_deg(deg, min, sec))
-        conv.set_ra(hour, min, sec)
-        assert equals(conv.get_ra(), conv.hms_to_deg(hour, min, sec))
-        conv.set_lon(deg, min, sec)
-        assert equals(conv.get_lon(), conv.dms_to_deg(deg, min, sec))
-        conv.set_lat(deg, min, sec)
-        assert equals(conv.get_lat(), conv.dms_to_deg(deg, min, sec))
-
-    conv.set_ra(ra, single_value=True)
-    conv.set_dec(dec, single_value=True)
-    conv.set_lon(lon, single_value=True)
-    conv.set_lat(lat, single_value=True)
-    assert equals(conv.get_ra(), ra)
-    assert equals(conv.get_dec(), dec)
-    assert equals(conv.get_lat(), lat)
-    assert equals(conv.get_lon(), lon)
-    conv.set_time(2023, 1, 2, 0, 0, 0)
-    assert equals(conv.get_time_jd(), 2459946.5)
-    time = conv.get_time()
-    conv.set_time_utc(time)
-    assert equals(time.jd, conv.get_time_jd())
-    print("test")
-
-
 def test_computation():
     time = conv.normal_time_to_utc(2020, 12, 23, 7, 34, 34.5).jd
     ra = conv.hms_to_deg(13, 37, 0.919)
@@ -90,50 +53,3 @@ def test_computation():
     assert az == "190d 48m 46.54s"
     assert el == "9d 30m 3.37s"
 
-
-def write(p, line):
-    assert p.poll() is None, "Program terminated early"
-    p.stdin.write(line + "\n")
-    p.stdin.flush()
-
-
-def read(p):
-    assert p.poll() is None, "Program terminated early"
-    line = p.stdout.readline()
-    return line
-
-
-def test_io():
-    for [command_name, function] in conv.executable_commands.items():
-        with subprocess.Popen(
-                "python3 converter.py -d",
-                shell=True,
-                stdout=subprocess.PIPE,
-                stdin=subprocess.PIPE,
-                universal_newlines=True,
-        ) as p:
-
-            num_lines = 3
-            # Not working
-            for i in range(num_lines):
-                line = p.stdout.readline()
-                #print(line)
-            if command_name == "ctime":
-                write(p, f"{command_name} 2022 10 10")
-            elif command_name == "rm":
-                write(p, f"{command_name} 1")
-                write(p, "y")
-            else:
-                write(p, f"{command_name} 10")
-            #print(command_name)
-            line = read(p)
-            #print(line)
-            assert (line != ""), "Read empty line or closed output pipe"
-
-            time.sleep(0.1)
-            assert p.poll() is None, "Program terminated early"
-            # write(p, "help")
-            p.stdin.close()
-
-
-test_io()
